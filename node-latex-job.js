0606bdd829364ceba8369c1f4827e98552156f3b
Tammo Laube <tammolaube@gmail.com>
2013-04-07 11:20:32 +0200

    further improvements

--- node-latex-job.js
+++ node-latex-job.js
@@ -1,109 +1,102 @@
-var 
-    path = require('path'),    
-    COMMANDS = {
-        PDFLATEX : 'pdflatex',
-        TEX : 'tex',
-        LATEX : 'latex'        
-    },
+var path = require('path'),    
     EXTENSIONS = {
-        TEX : '.tex',
-        LOG : '.log',
-        PDF : '.pdf',
-        DVI : '.dvi'
+        TEX : 'tex',
+        LOG : 'log',
+        PDF : 'pdf',
+        DVI : 'dvi'
     },
     PROGRAMS = {
         PDFLATEX : {
-            command : COMMANDS.PDFLATEX,
+            command : 'pdflatex',
             extension : EXTENSIONS.PDF
         },
         TEX : {
-            command : COMMANDS.TEX,
+            command : 'tex',
             extension : EXTENSIONS.DVI
         },
         LATEX : {
-            command : COMMANDS.LATEX,
+            command : 'latex',
             extension : EXTENSIONS.DVI
+        },
+        BIBTEX : {
+            command : 'bibtex'
+        },
+        MKINDEX : {
+            command : 'mkindex'
         }
     },
     // Job default options
-    DEFAULTS = (function () {
-        var 
-            program = PROGRAMS.PDFLATEX,
-            outputDir = '/tmp/node-latex/out',
-            done = 'false',
-            success = undefined,
-            message = 'Initial.',
-            
-            // private
-            jobName = undefined
-        ;
+    DEFAULTS = {
+        program : PROGRAMS.PDFLATEX,
+        outputDir : '/tmp/node-latex/out',
+        status : {
+            code : 0,
+            done : false,                
+            message : 'Initial.',
+            success : ''
+        },
+        getFileName : getFileName,
+        getInputFileName : getInputFileName,
+        getOutputFileName : getOutputFileName,
+        getOutputFilePath : getOutputFilePath,
+        getLogFilePath : getLogFilePath
+    };
 
-        function getInputFileName () {
-            return path.basename(inputFilePath);
-        }
+function getInputFileName () {
+    return path.basename(this.inputFilePath);
+}
 
-        function getJobName () {
-            var ret = jobName || path.basename(inputFilePath, EXTENSIONS.TEX);        
-            return ret;
-        }
+function getOutputFileName () {
+    return (this.program.extension) ? addExtension(this.jobName, this.program.extension) : '';
+}
 
-        function getOutputFilePath () {
-            return joinFilePath(program.extension);
-        }
+function getOutputFilePath () {
+    return (this.program.extension) ? joinFilePath.call(this, this.program.extension) : '';
+}
 
-        function getLogFilePath () { 
-            return joinFilePath(EXTENSIONS.LOG); 
-        }
+function getLogFilePath () {
+    return joinFilePath.call(this, EXTENSIONS.LOG); 
+}
 
-        function setJobName (name) { 
-            jobName = name; 
-        }
+function joinFilePath(extension) {
+    return path.join(this.outputDir, addExtension(this.jobName, extension)); 
+}
 
-        // private
-        function joinFilePath(extension) {
-            return path.join(outputDir, getJobName() + extension); 
-        }
+function getFileName(extension) {
+    return addExtension(this.jobName, extension);
+}
 
-        return {
-            program : program,
-            outputDir : outputDir,            
-            getInputFileName : getInputFileName,
-            getJobName : getJobName,
-            getOutputFilePath : getOutputFilePath,
-            getLogFilePath : getLogFilePath,
-            setJobName : setJobName,
-            status : {
-                done : done,
-                message : message,
-                success : success
-            }
-        }
-    })()
-;
+function addExtension(name, extension) {
+    return (extension) ? (name + '.' + extension) : name;
+}
 
 // Safely merges the new options or the defaults into the job
 function setOptions (job, options) {
     options = options || {};
-    // Set only attributes that are available public in DEFAULTS
     for (var attr in DEFAULTS) {
         job[attr] = options[attr] || DEFAULTS[attr];
+        delete options[attr];
     }
-    // Set private attribute(s)
-    job.setJobName(options.jobName);
+    for (var attr in options) {
+        job[attr] = options[attr];
+    }
     return job;
 }
 
 // Latex job constructor
 function Job(inputFilePath, options) {
-    if (!!inputFilePath) {
-        throw new Error('An inputFilePath must be defined!');
-    }    
+    if (!inputFilePath) {
+        throw new Error('Illegal Argument: An inputFilePath must be specified!');
+    }
 
     this.inputFilePath = inputFilePath || '';
+    this.jobName = path.basename(this.inputFilePath, addExtension('', EXTENSIONS.TEX));
+
     setOptions(this, options);
 }
 
+
 module.exports.PROGRAMS = PROGRAMS;
-module.exports.newJob = function job (inputFilePath, options) { 
+module.exports.newJob = function job (inputFilePath, options) {
     return new Job(inputFilePath, options);
 };
