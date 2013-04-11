77b58eaff0c8e7a5b8f76e2b49edc2fa576967e5
Tammo Laube <tammolaube@gmail.com>
2013-04-11 10:14:41 +0200

    added prototyping

--- tex.js
+++ tex.js
@@ -1,13 +1,15 @@
-var _ = require('underscore'), 
-    path = require('path'),
-    spawn = require('child_process').spawn,
-    EXTENSIONS = {
+var _ = require('underscore')
+  , path = require('path')
+  , spawn = require('child_process').spawn
+  , readline = require('readline')
+  , stream = require('stream')
+  , EXTENSIONS = {
         TEX : 'tex',
         LOG : 'log',
         PDF : 'pdf',
         DVI : 'dvi'
-    },
-    PROGRAMS = {
+    }
+  , PROGRAMS = {
         pdflatex : {
             command : 'pdflatex',
             extension : EXTENSIONS.PDF
@@ -30,8 +32,8 @@ var _ = require('underscore'),
         mkindex : {
             command : 'mkindex'
         }
-    },
-    DEFAULTS = {
+    }
+  , DEFAULTS = {
         program : PROGRAMS['pdflatex'],
         args : {
             interaction : 'batchmode'
@@ -44,6 +46,13 @@ var _ = require('underscore'),
         }
     };
 
+module.exports.PROGRAMS = PROGRAMS;
+module.exports.newJob = newJob;
+
+function newJob (input, arg1, arg2) {
+    return new Job(input, arg1, arg2);
+};
+
 // Latex job
 // Constructor signature: Job(input, [program], [options])
 //     input: Path to the .tex file to compile or a stream.Readable.
@@ -53,165 +62,170 @@ var _ = require('underscore'),
 //         .args: Use Job.getArgs() to get an array of tex args suitable for use with require('child_process').spawn(command, [args]).
 //         .options: Gets passed to require('child_process').spawn(command, [args], [options]).
 function Job(input, arg1, arg2) {
-    var privates = {};
+    this.privates = {};
 
     if (!input) {
         throw new Error('Illegal Argument: An input must be specified!');
     }
 
+    if (typeof input !== 'string' && !(input instanceof stream.Stream)) {
+        throw new Error('Input must be a path or a stream.Stream!');
+    }
+
     // Initialize with privates with arguments or defaults
     {
-        _.extend(privates, DEFAULTS, arg2);
+        _.extend(this.privates, DEFAULTS, arg2);
 
         if (typeof arg1 === 'string') {
-            privates.program = PROGRAMS[arg1];
+            this.privates.program = PROGRAMS[arg1];
         } else {
-            _.extend(privates, arg1) 
+            _.extend(this.privates, arg1) 
         }
 
         // Deep extension of privates with support of the defaults
         {
-            _.extend(privates.args, DEFAULTS.args, privates.args);
-            _.extend(privates.options, DEFAULTS.options, privates.options);
+            _.extend(this.privates.args, DEFAULTS.args, this.privates.args);
+            _.extend(this.privates.options, DEFAULTS.options, this.privates.options);
         }
 
-        privates.input = input;
-    }
-
-    this.getJobName = function () {
-        var jobname = privates.args.jobname;
-        if (!jobname) {
-            jobname = this.getInputFileName();
-            if (jobname) {
-                jobname = path.basename(jobname, '.' + EXTENSIONS.TEX);            
-            }
-        }
-        return jobname;
-    }
-
-    this.getFileName = function (extension) {
-        var jobname = this.getJobName();
-        return (jobname && extension) ? addExtension(jobname, extension) : undefined;
-    }
-
-    this.getInput = function () {
-        return privates.input;
-    }
-
-    this.getInputFileName = function () {
-        var inputFileName;
-        if (typeof privates.input === 'string') {
-            inputFileName = path.basename(privates.input);
-        }
-        return inputFileName;
-    }
-
-    this.getOutputExtension = function () {
-        return privates.program.extension;
-    }
-
-    this.getOutputFileName = function () {
-        return this.getFileName(privates.program.extension);
-    }
-
-    this.getOutputDirectory = function () {
-        return privates.args['output-directory'];
-    }
-
-    this.getOutputFilePath = function () {
-        return getOutputPath.call(this, this.getOutputFileName());
-    }
-
-    this.getLogFileName = function () {
-        return this.getFileName(EXTENSIONS.LOG);
-    }
-
-    this.getLogFilePath = function () {
-        return getOutputPath,call(this, this.getLogFileName());
-    }
-
-    this.getCommand = function () {
-        return privates.program.command;
-    }
-
-    this.getStatus = function () {
-        return privates.status;
-    }
-
-    // Converts the args object into an array compatible for *tex programs.
-    // Keys with the values { undefined, null, '' } are returned as '-key'.
-    // Keys with values are returned as '-key=value'.
-    this.getArgs = function () {
-        var ret = [];    
-        for (var attr in privates.args) {
-            var arg = '-' + attr,
-                val = privates.args[attr];
-            if (val) {
-                arg += '=' + val;
-            }        
-            ret.push(arg);
-        }
-        if (typeof privates.input === 'string') {
-            ret.push(privates.input);
-        }
-        return ret;
-    }
-
-    this.getOptions = function () {
-        return privates.options;
-    }
-
-    this.print = function (callback) {
-        var job = this,
-            printJob = startJob(this);
-
-        printJob.stdout.pipe(process.stdout);
-
-        if (typeof privates.input !== 'string' && privates.input.pipe !== 'undefined') {
-            try {
-                privates.input.pipe(printJob.stdin);
-            } catch (e) {
-                console.log('Error while piping input: \'' + e + '\'');
-                console.log('Trying to kill print job: \'' + job.getJobName() + '\'');
-                printJob.stdin.end();
-            }
-        }
-
-        printJob.on('close', function (code) {
-            console.log('Print job finished with code \'' + code + '\'');
-            privates.status.code = code;
-            if (typeof callback === 'function') {
-                callback(job, code == 0);
-            }
-        });
-
-        return undefined;
-    }
-
-    function getOutputPath(fileName) {
-        return (fileName) ? path.join(this.getOutputDirectory(), fileName) : undefined;
-    }
-
-    function addExtension(name, extension) {
-        var extendedName;
-        if (name) {
-            extendedName = name;
-        }
-        if (extension) {
-            extendedName += '.' + extension;
-        }
-        return extendedName;
-    }
-
-    function startJob(job) {
-        return spawn(job.getCommand(), job.getArgs(), job.getOptions());
+        this.privates.input = input;
     }
 
 }
 
-function newJob (input, arg1, arg2) {
-    return new Job(input, arg1, arg2);
-};
+Job.prototype.getJobName = function () {
+    var jobname = this.privates.args.jobname;
+    if (!jobname) {
+        jobname = this.getInputFileName();
+        if (jobname) {
+            jobname = path.basename(jobname, '.' + EXTENSIONS.TEX);            
+        }
+    }
+    return jobname;
+}
 
-module.exports.PROGRAMS = PROGRAMS;
-module.exports.newJob = newJob; 
+Job.prototype.getFileName = function (extension) {
+    var jobname = this.getJobName();
+    return (jobname && extension) ? addExtension(jobname, extension) : undefined;
+}
+
+Job.prototype.getInput = function () {
+    return this.privates.input;
+}
+
+Job.prototype.getInputFileName = function () {
+    var inputFileName;
+    if (typeof this.privates.input === 'string') {
+        inputFileName = path.basename(this.privates.input);
+    }
+    return inputFileName;
+}
+
+Job.prototype.getOutputExtension = function () {
+    return this.privates.program.extension;
+}
+
+Job.prototype.getOutputFileName = function () {
+    return this.getFileName(this.privates.program.extension);
+}
+
+Job.prototype.getOutputDirectory = function () {
+    return this.privates.args['output-directory'];
+}
+
+Job.prototype.getOutputFilePath = function () {
+    return getOutputPath.call(this, this.getOutputFileName());
+}
+
+Job.prototype.getLogFileName = function () {
+    return this.getFileName(EXTENSIONS.LOG);
+}
+
+Job.prototype.getLogFilePath = function () {
+    return getOutputPath,call(this, this.getLogFileName());
+}
+
+Job.prototype.getCommand = function () {
+    return this.privates.program.command;
+}
+
+Job.prototype.getStatus = function () {
+    return this.privates.status;
+}
+
+// Converts the args object into an array compatible for *tex programs.
+// Keys with the values { undefined, null, '' } are returned as '-key'.
+// Keys with values are returned as '-key=value'.
+Job.prototype.getArgs = function () {
+    var ret = [];    
+    for (var attr in this.privates.args) {
+        var arg = '-' + attr,
+            val = this.privates.args[attr];
+        if (val) {
+            arg += '=' + val;
+        }        
+        ret.push(arg);
+    }
+    if (typeof this.privates.input === 'string') {
+        ret.push(this.privates.input);
+    }
+    return ret;
+}
+
+Job.prototype.getOptions = function () {
+    return this.privates.options;
+}
+
+Job.prototype.print = function (callback) {
+    var job = this,
+        printJob = startJob(this);
+
+    printJob.stdout.pipe(process.stdout);
+
+    if (typeof this.privates.input !== 'string' && this.privates.input.pipe !== 'undefined') {
+        try {
+            var rl = readline.createInterface({
+                input: this.privates.input,
+                output: process.stdout
+            });
+
+            rl.on('line', function (line) {
+              printJob.stdin.write(line);
+            });
+
+        } catch (e) {
+            console.log('Error while printing input: \'' + e + '\'');
+            console.log('Trying to kill print job: \'' + job.getJobName() + '\'');
+            printJob.stdin.end();
+        }
+    }
+
+    printJob.on('close', function (code) {
+        console.log('Print job finished with code \'' + code + '\'');
+        job.privates.status.code = code;
+        if (typeof callback === 'function') {
+            callback(job, code == 0);
+        }
+    });
+
+    return undefined;
+}
+
+function getOutputPath(fileName) {
+    return (fileName) ? path.join(this.getOutputDirectory(), fileName) : undefined;
+}
+
+function addExtension(name, extension) {
+    var extendedName;
+    if (name) {
+        extendedName = name;
+    }
+    if (extension) {
+        extendedName += '.' + extension;
+    }
+    return extendedName;
+}
+
+function startJob(job) {
+    return spawn(job.getCommand(), job.getArgs(), job.getOptions());
+}
